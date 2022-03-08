1164f77aa6a49a69abae67159ff3ce4caa860257
Markw1998 <S00181377@mail.itsligo.ie>
2022-03-08 17:33:37 +0000

    Added ListBox view and Enum added. Can add to list.

---
+++ EmpHoursCalculator/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmpHoursCalculator
+{
+     class Employee
+    {
+        public string EmpName { get; set; }
+        public int EmpHours { get; set; }
+        public int EmpBreakTime { get; set; }
+
+        public Employee(string _employeeName, int _empHours)
+        {
+            this.EmpName = _employeeName;
+            this.EmpHours = _empHours;
+            this.EmpBreakTime = 0;
+
+        }
+
+        //Calculates Breaks Based of Day/Normal Shift
+        public virtual int CalculateBreaks(int hoursWorked)
+        {
+            int breakTime = hoursWorked * 10;
+
+            int numberOf = (hoursWorked * 60) / 240;
+
+            int additionalMinutes = numberOf * 20;
+
+
+            // Break out and return the value
+            return breakTime + additionalMinutes;
+        }
+
+        public override string ToString()
+        {
+            return String.Format($"Employee name: {EmpName}\nEmployee working hours: {EmpHours}\nEmployee break entitlement: {CalculateBreaks(EmpHours)} minutes \n\n ");
+        }
+    }
+}
--- EmpHoursCalculator/MainWindow.xaml.cs
+++ EmpHoursCalculator/MainWindow.xaml.cs
@@ -13,17 +13,60 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Text.RegularExpressions;
+using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace EmpHoursCalculator
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow
     {
+        private ObservableCollection<Employee> ListOfEmployees = new ObservableCollection<Employee>();
+        private enum ShiftType
+        {
+            [Display(Name = "Day Shift")]
+            DayShift,
+            [Display(Name = "Night Shift")]
+            NightShift,
+            [Display(Name = "Split Shift")]
+            SplitShift
+        };
         public MainWindow()
         {
             InitializeComponent();
+            lstNames.ItemsSource = ListOfEmployees;
+            empShiftTypeInput.ItemsSource = Enum.GetValues(typeof(ShiftType)).Cast<ShiftType>();
+        }
+
+
+
+
+        private void NumberValidationTextBox(object sender, TextCompositionEventArgs e)
+        {
+            Regex regex = new Regex("[^0-9]+");
+            e.Handled = regex.IsMatch(e.Text);
+        }
+
+        private void btnAdd_Click(object sender, RoutedEventArgs e)
+        {
+            string name;
+            int hours;
+            int shiftType;
+
+            name = empNameInput.Text;
+            hours = int.Parse(empHoursInput.Text);
+            shiftType = (int)empShiftTypeInput.SelectedValue;
+
+
+
+
+            if (name != null && hours != null && shiftType != null)
+            {
+                Employee dayEmp = new Employee(name, hours);
+
+                ListOfEmployees.Add(dayEmp);
+
+            }
         }
     }
 }
